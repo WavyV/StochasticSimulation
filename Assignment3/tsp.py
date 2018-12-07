04959f3d6de0b5e8047f7c6b6e6208f26990e66a
Daan van Ingen <daanvaningen@Daans-MacBook-Pro.local>
2018-12-07 18:15:14 +0100

    implemented greedy start

--- Assignment3/tsp.py
+++ Assignment3/tsp.py
@@ -18,10 +18,24 @@ def proposeNewTour(tour):
 
     return tour
 
-distM = np.load('distM/distMeil51.npy')
-tour = [i for i in range(51)]
-random.shuffle(tour) # take a random starting point
+def greedyTour(size):
+    tour = [random.randint(0, size-1)]
+    for _ in range(size-1):
+        minIndex, minValue = 0, 100000
+        for i in range(size):
+            if tour[-1] == i or i in tour:
+                continue
+            elif distM[tour[-1]][i] < minValue:
+                minIndex, minValue = i, distM[tour[-1]][i]
+        tour.append(minIndex)
 
+    return tour
+
+distM = np.load('distM/distMeil51.npy')
+# tour = [i for i in range(51)]
+# random.shuffle(tour) # take a random starting point
+tour = greedyTour(51)
+print(tour)
 opt = [1,22,8,26,31,28,3,36,35,20,2,29,21,16,50,34,30,9,49,10,39,33,45,15,44,42,
     40,19,41,13,25,14,24,43,7,23,48,6,27,51,46,12,47,18,4,17,37,5,38,11,32]
 opt = [opt[i]-1 for i in range(len(opt))]
@@ -29,10 +43,10 @@ opt = [opt[i]-1 for i in range(len(opt))]
 # print(opt)
 
 # parameters
-temperature = 100000
+temperature = 200
 # stopTemperature = 0.00000001
-stopIteration = 200000
-alpha = 0.999
+stopIteration = 1000
+alpha = 0.98
 delta = 0.1
 
 curScore = getTourScore(tour)
@@ -62,10 +76,12 @@ while iteration < stopIteration:
 
     if(iteration % 100 == 0):
         print(curScore, temperature)
-    if(notImproved < 50):
-        temperature *= alpha
-    else:
-        temperature *= 1./alpha
+
+    temperature = temperature*alpha
+    # if(notImproved < 50):
+    #     temperature *= alpha
+    # else:
+    #     temperature *= 1./alpha
     # temperature = temperature*(1+math.log(1+delta)*temperature/3*np.std(scores))**-1
     iteration += 1
 
