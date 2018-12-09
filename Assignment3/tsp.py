94786c1fd1f545fd9222bb5fa37bd776cc0765bd
Daan van Ingen <daanvaningen@Daans-MacBook-Pro.local>
2018-12-09 15:35:40 +0100

    different parameter settings

--- Assignment3/tsp.py
+++ Assignment3/tsp.py
@@ -15,7 +15,6 @@ def proposeNewTour(tour):
     rand1 = random.randint(0, len(tour)-1)
     rand2 = random.randint(rand1+1, len(tour)) # rand1 < rand2
     tour[rand1:rand2] = reversed(tour[rand1:rand2])
-
     return tour
 
 def greedyTour(size):
@@ -35,7 +34,6 @@ distM = np.load('distM/distMeil51.npy')
 # tour = [i for i in range(51)]
 # random.shuffle(tour) # take a random starting point
 tour = greedyTour(51)
-print(tour)
 opt = [1,22,8,26,31,28,3,36,35,20,2,29,21,16,50,34,30,9,49,10,39,33,45,15,44,42,
     40,19,41,13,25,14,24,43,7,23,48,6,27,51,46,12,47,18,4,17,37,5,38,11,32]
 opt = [opt[i]-1 for i in range(len(opt))]
@@ -43,10 +41,10 @@ opt = [opt[i]-1 for i in range(len(opt))]
 # print(opt)
 
 # parameters
-temperature = 200
+temperature = 10**3
 # stopTemperature = 0.00000001
-stopIteration = 1000
-alpha = 0.98
+stopIteration = 1
+alpha = 0.9999
 delta = 0.1
 
 curScore = getTourScore(tour)
